95ab8a8fdfd2cadda336a2f30606ef8176474aa1
Miguel R. Araújo <miguel.r.araujo16@gmail.com>
2026-09-13 14:20:16 -0300

    ver 2.0

---
+++ hello-miguel.py
@@ -0,0 +1,5 @@
+print("hello, world")
+
+print("hello, moon")
+
+print("hello, sun")
--- hello.py
+++
@@ -1,9 +0,0 @@
-print("hello, world")
-
-print("how are you?")
-
-print("I'm fine, thank you")
-
-print("good bye, world")
-
-print("see you later")
